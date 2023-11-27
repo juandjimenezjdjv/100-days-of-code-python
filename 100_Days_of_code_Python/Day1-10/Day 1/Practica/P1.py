4d426f020df2aa5dd2ef9d66eaf3a81c938580fc
juandjimenezjdjv <123849198+juandjimenezjdjv@users.noreply.github.com>
2023-11-27 11:18:17 -0600

    Add files via upload

---
+++ 100_Days_of_code_Python/Day1-10/Day 1/Practica/P1.py
@@ -0,0 +1,41 @@
+score = 67
+if score < 80 and score > 70:
+    print("A")
+elif score < 90 or score > 80:
+    print("B")
+elif score > 60:
+    print("C")
+else:
+    print("D")
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+result = [num + 3 for num in numbers if num % 2 == 0]
+print(result)
+
+##def a_function(a_parameter):    
+##    a_variable = 15    
+##    return a_parameter 
+##a_function(10)
+##print(a_variable)
+
+def outer_function(a, b):
+    def inner_function(c, d):
+        return c + d
+    return inner_function(a, b)
+ 
+result = outer_function(5, 10)
+print(result)
+
+print("_______")
+def foo(a, b=4, c=6):
+    print(a, b, c)
+ 
+foo(20)
+print("_______")
+
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+ 
+all_aboard(4, 7, 3, 0, x=10, y=64)
+
+quit()
---
+++ 100_Days_of_code_Python/Day1-10/Day 1/Practica/P2.py
@@ -0,0 +1,10 @@
+city = "ubi"
+name = "lola"       
+
+print ("Welcome to the Band Name Generator.")
+print ("What's the name of the city you grew up in?")
+city = input ()
+print ("What's your pet name?")
+name = input ()
+print ("Your band name could be",city, name)
+quit()
